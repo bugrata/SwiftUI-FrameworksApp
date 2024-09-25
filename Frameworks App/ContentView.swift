65c97430db4ab79f423730f61d1a2f3f5c2c70a9
Buğrahan <bugraata2344@gmail.com>
2024-09-25 21:36:33 +0300

    Initial Commit

---
+++ Frameworks App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Frameworks App
+//
+//  Created by Buğrahan Ata on 25.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Frameworks App/Frameworks_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Frameworks_AppApp.swift
+//  Frameworks App
+//
+//  Created by Buğrahan Ata on 25.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct Frameworks_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
